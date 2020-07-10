507a80a5120091afbd8d0650382d16714766873f
Jiahua Liao <ljh15088131916@gmail.com>
2020-07-10 14:46:30 -0400

    update 1.7ex

--- part1/src/index.js
+++ part1/src/index.js
@@ -185,11 +185,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [All, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(All + 1)
+    setAverage((parseInt(good+1)-parseInt(bad))/parseInt(All+1))
+    setPositive(parseInt(good+1)/parseInt(All+1))
+  }
 
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(All + 1)
+    setAverage((parseInt(good)-parseInt(bad))/parseInt(All+1))
+    setPositive(parseInt(good)/parseInt(All+1))
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(All + 1)
+    setAverage((parseInt(good)-parseInt(bad+1))/parseInt(All+1))
+    setPositive(parseInt(good)/parseInt(All+1))
+  }
   return (
     <div>
       <h1> Give Feedback </h1>
@@ -200,6 +218,9 @@ const App = () => {
       <Statistics text='Good' counter={good} />
       <Statistics text='Neutral' counter={neutral} />
       <Statistics text='Bad' counter={bad} />
+      <Statistics text='All' counter={All} />
+      <Statistics text='Average' counter={average} />
+      <Statistics text='Positive' counter={positive} />
     </div>
   )
 }
